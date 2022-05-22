861d9b57a40dd9bd4401e008dab7b0355f2b8a77
msjun23 <msjun23@gmail.com>
2022-05-22 15:43:37 +0900

    Update mapping parameter

--- umrobot_cartographer/configuration_files/umrobot.lua
+++ umrobot_cartographer/configuration_files/umrobot.lua
@@ -48,10 +48,10 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.min_range = 0.3
 TRAJECTORY_BUILDER_2D.max_range = 40.0
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
-POSE_GRAPH.constraint_builder.min_score = 0.85                      --0.65
+POSE_GRAPH.constraint_builder.min_score = 0.55                      --0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.6   --0,7
 return options
